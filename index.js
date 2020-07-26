bea0451b9ca7f9d28303490078ce96229b4bae0b
cwissy <meahchris@gmail.com>
2020-07-26 15:10:16 +0100

    update to questions

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 // Make a Queue class to manage a queue system with two methods, add and serve - first in, first out
+// When the Queue is empty, and you try to serve, then an error should be thrown saying "Queue is empty"
 
 class Queue {}
 
 // Make a Stack class to manage a stack system with two methods, add and serve - last in, last out
+// When the Stack is empty, and you try to serve, then an error should be thrown saying "Stack is empty"
 
 class Stack {}
 
 // Make a PriorityQueue class to manage a queue system with two methods, add and serve - served in order of priority
 // The add method should now take in 2 arguments, first the item, and second the priority which will be a number (the higher the number, the higher the priority)
+// When the PriorityQueue is empty, and you try to serve, then an error should be thrown saying "PriorityQueue is empty"
 
 class PriorityQueue {}
 
