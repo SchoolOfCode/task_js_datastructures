fdafd63c981ba864522e3956159b5a17e67bc5c9
cwissy <meahchris@gmail.com>
2020-07-26 15:11:46 +0100

    default priority question

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ class Stack {}
 
 // Make a PriorityQueue class to manage a queue system with two methods, add and serve - served in order of priority
 // The add method should now take in 2 arguments, first the item, and second the priority which will be a number (the higher the number, the higher the priority)
+// The PriorityQueue should still function even if only the item is given as an argument in the add method (think default priority)
 // When the PriorityQueue is empty, and you try to serve, then an error should be thrown saying "PriorityQueue is empty"
 
 class PriorityQueue {}
